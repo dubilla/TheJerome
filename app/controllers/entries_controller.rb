86045b10ff59237e9b22cf3fc8ed4d9c04b0bac7
dubilla <dan.ubilla@gmail.com>
2019-02-23 10:56:21 -0500

    Add entry pages

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,31 @@
 class EntriesController < ApplicationController
   def show
+    @entry = Entry.find(params[:id])
+  end
+
+  def edit
+    @tournaments = [
+      {
+        name: 'American',
+        teams: [
+          'Central Florida',
+          'Memphis',
+          'South Florida'
+        ]
+      },
+      {
+        name: 'ACC',
+        teams: []
+      },
+      {
+        name: 'Atlantic 10',
+        teams: []
+      }
+    ]
+    @entry = Entry.find(params[:id])
+  end
+
+  def update
+    @entry = Entry.find(params[:id])
   end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,5 @@
 class Entry < ApplicationRecord
   belongs_to :user
   has_many :picks
+  accepts_nested_attributes_for :picks
 end
