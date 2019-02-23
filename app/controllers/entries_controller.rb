02e7fd25d491203149ea8b0513e2da903b7e85c1
dubilla <dan.ubilla@gmail.com>
2019-02-23 14:49:44 -0500

    Allow for entries to be edited

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -9,5 +9,18 @@ class EntriesController < ApplicationController
 
   def update
     @entry = Entry.find(params[:id])
+    if @entry.update_attributes(entry_params)
+      redirect_to @entry
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def entry_params
+    params
+      .required(:entry)
+      .permit(picks_attributes: [:id, :team_id, :tournament_id])
   end
 end
--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,3 +1,4 @@
 class Tournament < ApplicationRecord
   has_many :teams, inverse_of: :tournament
+  has_many :picks
 end
