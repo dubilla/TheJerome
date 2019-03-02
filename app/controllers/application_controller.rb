6901e5d90919e0816989b90e450747e5ca28f7fe
dubilla <dan.ubilla@gmail.com>
2019-03-02 11:57:34 -0500

    Add redirection for unauthorized users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   include Pundit
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to view this page"
+    redirect_to root_path
+  end
 end
