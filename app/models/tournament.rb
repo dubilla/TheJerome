8b36c3547bedfe8780989da7b7097a089881bfd0
dubilla <dan.ubilla@gmail.com>
2019-03-04 22:04:34 -0500

    Add proper times to entry pages

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -5,4 +5,9 @@ class Tournament < ApplicationRecord
   def locked?
     starts_at < DateTime.current
   end
+
+  def starts_at_est
+    zone = ActiveSupport::TimeZone.new("Eastern Time (US & Canada)")
+    starts_at.in_time_zone(zone)
+  end
 end
