fb7d90506eb862b24e2bb39578eae95c3e3eb50f
dubilla <dan.ubilla@gmail.com>
2019-03-09 12:56:07 -0500

    Add entry names and display on tournament pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_025611) do
+ActiveRecord::Schema.define(version: 2019_03_09_175419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_03_07_025611) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["user_id"], name: "index_entries_on_user_id"
   end
 
