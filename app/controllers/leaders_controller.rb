683f50c0917b612544495aef2547ce077e4cef29
dubilla <dan.ubilla@gmail.com>
2019-03-13 21:26:25 -0400

    Style finals and champion picks more clearly

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -1,5 +1,5 @@
 class LeadersController < ApplicationController
   def index
-    @leaders = Entry.all.sort_by{|e| [-e.points_possible_remaining, -e.score] }
+    @leaders = Entry.all.sort_by{|e| [-e.score, -e.points_possible_remaining] }
   end
 end
