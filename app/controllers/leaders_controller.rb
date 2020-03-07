5260be6029d732570de89eac4d53fb6bb125bf6b
dubilla <dan.ubilla@gmail.com>
2020-03-06 21:42:29 -0500

    Fix bug to allow for leaderboard to work

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -1,11 +1,12 @@
 class LeadersController < ApplicationController
   def index
-    @leaders = Entry.joins(:year).where(years: { name: params[:year] }).sort_by{|e| [-e.score, -e.points_possible_remaining] }
+    @leaders = Entry.joins(:year).where(years: { name: index_params[:name] }).sort_by{|e| [-e.score, -e.points_possible_remaining] }
   end
 
   private
 
   def index_params
-    params.fetch(:year, {name: '2020'})
+    params
+      .fetch(:year, {name: '2020'})
   end
 end
