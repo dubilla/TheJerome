17f46aaf11735aede143cea9d2dec2a83abe33cb
dubilla <dan.ubilla@gmail.com>
2020-03-06 22:35:57 -0500

    Fix n+1 on entry show

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,7 +6,7 @@ class EntriesController < ApplicationController
   end
 
   def show
-    @entry = Entry.includes(:user, picks: [:tournament, :team]).order("tournaments.starts_at").find(params[:id])
+    @entry = Entry.includes(:user, picks: [:tournament, team: :round]).order("tournaments.starts_at").find(params[:id])
   end
 
   def edit
