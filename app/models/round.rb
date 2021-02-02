e4c7875819c594897e0432aea7b3c5c1938a0ac6
dubilla <dan.ubilla@gmail.com>
2021-02-02 17:00:20 -0500

    Add chrome headless support to system specs

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,3 +1,4 @@
 class Round < ApplicationRecord
   has_many :teams
+  validates :order, presence: true, numericality: { only_integer: true }
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,6 @@ class Team < ApplicationRecord
   belongs_to :tournament
   belongs_to :round
   has_many :picks
+
+  validates :name, presence: true
 end
