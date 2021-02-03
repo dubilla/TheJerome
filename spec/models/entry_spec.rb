1ca7ff72f45add95e371df7aa30f1a335002c758
dubilla <dan.ubilla@gmail.com>
2021-02-02 20:58:23 -0500

    Add some more coverage to the entry model

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -7,4 +7,10 @@ describe 'Entry' do
       expect(entry.score).to eq 0
     end
   end
+  describe '#points_possible_remaining' do
+    let(:entry) { Entry.new }
+    it 'returns a max points possible remaining' do
+      expect(entry.points_possible_remaining).to eq 0
+    end
+  end
 end
